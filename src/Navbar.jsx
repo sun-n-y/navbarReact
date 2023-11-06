28b2256ee1873ceb8c33a7ca08200c0abb202293
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-06 13:02:06 -0500

    add navbar complete

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -44,6 +44,16 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );
