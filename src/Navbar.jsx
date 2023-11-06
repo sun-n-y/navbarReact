1c00f12782fb2ed925a7ec0b308b2ff7d0dd519b
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-06 12:11:12 -0500

    add intial navbar component

---
+++ src/Navbar.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        {showLinks && (
+          <div className="links-container">
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
